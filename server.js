4c7c66828afcf9538cb2fac99988376257a3074c
yuriy <yuriy@dustcase.com>
2016-06-17 18:07:09 +0300

    Server UPD node_modules added

--- server.js
+++ server.js
@@ -1,24 +1,63 @@
-//Lets require/import the HTTP module
-var http = require('http');
+var PORT = 3000;
 
-//Lets define a port we want to listen to
-const PORT = 8080; 
+// load koa
+var koa = require('koa');
+var app = new koa();
+var router = require('koa-router')();
 
-//We need a function which handles requests and send response
-function handleRequest(request, response)
-{
-    response.end(Math.floor(Math.random() * 10.0).toString());
-}
+// CORS for cross-domain request
+var cors = require('koa-cors');
+app.use(cors());
 
-//Create a server
-var server = http.createServer(handleRequest);
 
-//Lets start our server
-server.listen(
-	PORT, 
-	function()
-	{
-    	//Callback triggered when server is successfully listening. Hurray!
-    	console.log("Server listening on: http://localhost:%s", PORT);
-	}
-);
+// routers
+
+router.get('/', function *(next) {
+    this.body = 'Hello World!';
+
+    yield next;
+});
+
+router.get('/LobbyBonus', function *(next) {
+    this.body = (Math.floor(Math.random() * 10.0).toString());
+
+    yield next;
+});
+
+
+
+// add routes
+app
+    .use(router.routes())
+    .use(router.allowedMethods());
+
+
+// koa error handlers
+process.on('uncaughtException', function (error) {
+
+    if (error.syscall !== 'listen') {
+        console.error(error);
+        process.exit(1);
+    }
+
+    var bind = 'Port ' + PORT;
+
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges');
+            process.exit();
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use');
+            process.exit();
+            break;
+        default:
+            throw error;
+    }
+});
+
+//start service
+app.listen(PORT, function () {
+    console.log(`API service listen http://localhost:${PORT}`);
+});
